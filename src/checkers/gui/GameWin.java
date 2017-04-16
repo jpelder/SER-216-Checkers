3e4caaabd6c170f0c8269d8ca5bd19ac3054be93
Wesley Davis <wesleydavis@Wesleys-MacBook-Pro.local>
2017-04-16 16:57:10 -0700

    Did not properly add the files, read ReadME

---
+++ src/checkers/gui/GameWin.java
@@ -0,0 +1,32 @@
+package checkers.gui;
+
+import javax.swing.JLabel;
+import javax.swing.JDialog;
+
+import java.awt.Point;
+import java.awt.Font;
+
+public class GameWin extends JDialog{
+    Point p;
+    JLabel masseage = new JLabel();
+    
+    GameWin(String winner, Point p){
+        this.p = p;
+        masseage.setText("          " + winner + " Wins!");
+        setupGUI();
+	}
+
+	private void setupGUI(){
+        new PlaySound("sounds/Win.wav").start();
+        masseage.setFont(new Font("dialog", Font.BOLD, 16));
+        add(masseage);
+
+        setAlwaysOnTop(true);
+        setLocation((int)p.getX()+100, (int)p.getY() + 200);
+        setResizable(false);
+        setSize(200, 80);
+        setTitle("Game Over");
+        setVisible(true);
+        setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+    }
+}
---
+++ src/checkers/gui/IntelliChecker.java
@@ -0,0 +1,13 @@
+package checkers.gui;
+
+
+
+public class IntelliChecker {
+    public static void main(String args[]) throws Exception {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+        	public void run() {
+        		new CheckerFrame();
+        	}
+        });
+    }
+}
